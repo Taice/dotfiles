04d36370dda32f4fe7bcaa1a1d925cf85fac42ff
Taice <tai.stanic@gmail.com>
2025-08-01 01:39:25 +0200

    update

--- .config/nvim/plugin/comp-commands.lua
+++ .config/nvim/plugin/comp-commands.lua
@@ -1,11 +1,45 @@
+local function open_floating_window(opts)
+    local opts = opts or {}
+	local ui = vim.api.nvim_list_uis()[1]
+	local win_width = ui.width
+	local win_height = ui.height
+
+	-- Default to 80% of the window size if width/height not provided
+	local width = opts.width or math.floor(win_width * 0.8)
+	local height = opts.height or math.floor(win_height * 0.8)
+
+	-- Center the floating window
+	local row = math.floor((win_height - height) / 3)
+	local col = math.floor((win_width - width) / 2)
+
+	-- Create a new empty buffer
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	-- Define window options
+	local opts = {
+		style = "minimal",
+		relative = "editor",
+		width = width,
+		height = height,
+		row = row,
+		col = col,
+		border = "rounded",
+	}
+
+	-- Open the floating window
+	local win = vim.api.nvim_open_win(buf, true, opts)
+	return { buf = buf, win = win }
+end
+
 -- Global variables to hold session-specific commands
 local run_cmd = ""
-local comp_cmd = ""
+local compile_cmd = ""
 
 -- Set up keymaps to use the configured commands
 vim.keymap.set("n", "<leader>cr", function()
   if run_cmd ~= "" then
-    vim.cmd("!" .. run_cmd)
+    open_floating_window()
+    vim.cmd(".!" .. run_cmd)
   else
     print("Run command not set. Use :ConfRun to configure.")
   end
@@ -13,7 +47,8 @@ end, { desc = "Run configured command" })
 
 vim.keymap.set("n", "<leader>cc", function()
   if compile_cmd ~= "" then
-    vim.cmd("!" .. compile_cmd)
+    open_floating_window()
+    vim.cmd(".!" .. compile_cmd)
   else
     print("Compile command not set. Use :ConfCompile to configure.")
   end
